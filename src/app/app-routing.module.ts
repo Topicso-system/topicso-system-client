c2d26e3786c5a041d759d67ab73d75d7c0563bd8
Norbert Fabian <fabian.norbert@hotmail.sk>
2019-11-30 10:14:07 +0100

    "Adding videos route to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VideosComponent } from './videos/videos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'videos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'videos',
+    component: VideosComponent,
+    data: { title: 'Videos' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
