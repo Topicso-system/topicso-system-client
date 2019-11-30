041628e7a25b52a5f7bb3741fe6b4344bfa89adc
Norbert Fabian <fabian.norbert@hotmail.sk>
2019-11-30 15:56:13 +0100

    Adding SafePipe for protecting against XSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { SafePipe } from './shared/pipes/SafePipe';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,8 @@ import { VideosComponent } from './videos/videos.component';
 @NgModule({
   declarations: [
     AppComponent,
-    VideosComponent
+    VideosComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
