f3f49f36ebca94096fdafdf289921fe202ca3740
Norbert Fabian <fabian.norbert@hotmail.sk>
2019-11-30 10:51:29 +0100

    Implementing VideoService#getAll()

--- src/app/shared/video/video.service.ts
+++ src/app/shared/video/video.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+
+  getAll(): Observable<any> {
+    return this.http.get('//localhost:8080/videos');
+  }
 }
