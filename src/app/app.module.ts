cb07639451fe597013c78e899b50257d24aecd81
Norbert Fabian <fabian.norbert@hotmail.sk>
2019-12-01 22:18:29 +0100

    Adding PageNotFoundComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,24 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './other/pageNotFound.component';
 import { ListVideosComponent } from './list-videos/list-videos.component';
 import { WatchVideoComponent } from './watch-video/watch-video.component';
 import { SafePipe } from './shared/pipes/SafePipe';
+import { from } from 'rxjs';
 
 const appRoutes: Routes = [
-  { path: 'watchVideo', component: WatchVideoComponent }
+  { path: 'watchVideo', component: WatchVideoComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
     ListVideosComponent,
     SafePipe,
-    WatchVideoComponent
+    WatchVideoComponent.
   ],
   imports: [
     BrowserModule,
