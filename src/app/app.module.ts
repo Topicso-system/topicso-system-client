1cd243a012cf720106b54e3e2352c41ee3c24d31
Norbert Fabian <fabian.norbert@hotmail.sk>
2019-12-01 22:03:32 +0100

    Rearanging components in app.module so local components are together

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { SafePipe } from './shared/pipes/SafePipe';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListVideosComponent } from './list-videos/list-videos.component';
 import { WatchVideoComponent } from './watch-video/watch-video.component';
+import { SafePipe } from './shared/pipes/SafePipe';
+
 
 @NgModule({
   declarations: [
