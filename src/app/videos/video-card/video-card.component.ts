be4d54e7a8bad44b1138b7dd2189cab084d8468f
Norbert Fabian <fabian.norbert@hotmail.sk>
2019-12-02 19:07:39 +0100

    Moving VideoCard from list-video to video-card component

--- src/app/videos/video-card/video-card.component.ts
+++ src/app/videos/video-card/video-card.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-video-card',
   templateUrl: './video-card.component.html',
-  styleUrls: ['./video-card.component.sass']
+  styleUrls: ['./video-card.component.sass'],
+  inputs: ['video']
 })
 export class VideoCardComponent implements OnInit {
 
