5fa2c95754a00f67f2a60e615a3f962dd9c32e55
Norbert Fabian <fabian.norbert@hotmail.sk>
2020-04-04 12:12:58 +0200

    Change input object in VideoCardComponent to input via annotation

--- src/app/videos/video-card/video-card.component.ts
+++ src/app/videos/video-card/video-card.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-video-card',
   templateUrl: './video-card.component.html',
-  styleUrls: ['./video-card.component.sass'],
-  inputs: ['video']
+  styleUrls: ['./video-card.component.sass']
 })
 export class VideoCardComponent implements OnInit {
+
+  @Input('video')
+  video: any = {};
 
   constructor() { }
 
