4f7c6fa54901c290f2455ba1629ac7fcdb0a24b0
Norbert Fabian <fabian.norbert@hotmail.sk>
2020-04-06 18:36:05 +0200

    Move navigation component to shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { WatchVideoComponent } from './videos/watch-video/watch-video.component'
 import { SafePipe } from './shared/pipes/safe/safe.pipe';
 import { VideoFilterComponent } from './videos/video-filter/video-filter.component';
 import { VideoCardComponent } from './videos/video-card/video-card.component';
-import { NavigationComponent } from './core/navigation/navigation.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
 import { environment } from 'src/environments/environment';
 
 const appRoutes: Routes = [
